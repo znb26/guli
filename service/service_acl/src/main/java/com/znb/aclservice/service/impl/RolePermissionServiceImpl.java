4d6ac23637aa6acc05d017ad2ffe8dbe35ba1ec1
znb <znb20010119@163.com>
2022-06-21 00:05:18 +0800

    fix 第十七天 整合gateway，实现权限管理模块接口

---
+++ service/service_acl/src/main/java/com/znb/aclservice/service/impl/RolePermissionServiceImpl.java
@@ -0,0 +1,21 @@
+package com.znb.aclservice.service.impl;
+
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.znb.aclservice.entity.RolePermission;
+import com.znb.aclservice.mapper.RolePermissionMapper;
+import com.znb.aclservice.service.RolePermissionService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 角色权限 服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Service
+public class RolePermissionServiceImpl extends ServiceImpl<RolePermissionMapper, RolePermission> implements RolePermissionService {
+
+}
---
+++ service/service_acl/src/main/java/com/znb/aclservice/service/impl/UserRoleServiceImpl.java
@@ -0,0 +1,21 @@
+package com.znb.aclservice.service.impl;
+
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.znb.aclservice.entity.UserRole;
+import com.znb.aclservice.mapper.UserRoleMapper;
+import com.znb.aclservice.service.UserRoleService;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author testjava
+ * @since 2020-01-12
+ */
+@Service
+public class UserRoleServiceImpl extends ServiceImpl<UserRoleMapper, UserRole> implements UserRoleService {
+
+}
